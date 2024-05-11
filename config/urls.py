dc210a24e06b4b3ed69156bb1f4d5f1ebffbe1e8
Danil Klimenko <sos.danil@yandex.ru>
2024-05-11 17:34:03 +0500

    настроил урлы в общем конфиге, добавил пустые файлы по все приложения

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('newsletter.urls', namespace='newsletter')),
+    path('clients/', include('clients.urls', namespace='clients')),
+    path('texts/', include('texts.urls', namespace='texts')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
