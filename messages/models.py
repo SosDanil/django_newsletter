51842185b28e1c4d37819cad1a5b35345daddad9
Danil Klimenko <sos.danil@yandex.ru>
2024-05-19 13:18:22 +0500

    создал модель сообщения

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    subject = models.CharField(max_length=300, verbose_name='тема письма')
+    body = models.TextField(verbose_name='тело письма', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.subject}'
+
+    class Meta:
+        verbose_name = 'сообщение'
+        verbose_name_plural = 'сообщения'
---
+++ messages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from messages.apps import MessagesConfig
+
+
+app_name = MessagesConfig.name
+
+# Пустые кавычки - идем от корня
+urlpatterns = [
+]
+
