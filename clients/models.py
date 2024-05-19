18e02b8da50c1e97e7cb2a2ddcf2464ce7b4a217
Danil Klimenko <sos.danil@yandex.ru>
2024-05-19 13:34:09 +0500

    создал модель клиента

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    FEMALE = 'female'
+    MALE = 'male'
+
+    GENDERS = (
+        (FEMALE, 'женщина'),
+        (MALE, 'мужчина'),
+    )
+
+    full_name = models.CharField(max_length=200, verbose_name='Ф.И.О.')
+    email = models.EmailField(verbose_name='Почта')
+    gender = models.CharField(max_length=6, choices=GENDERS, verbose_name='Пол клиента')
+    age = models.SmallIntegerField(verbose_name='Возраст', null=True, blank=True)
+    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.full_name} ({self.email})'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+        ordering = ('full_name', )
---
+++ clients/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from clients.apps import ClientsConfig
+
+
+app_name = ClientsConfig.name
+
+# Пустые кавычки - идем от корня
+urlpatterns = [
+]
