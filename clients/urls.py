e37d82563dab47cfb04fc39991afbb167870e6e6
Danil Klimenko <sos.danil@yandex.ru>
2024-05-21 16:54:30 +0500

    CRUD for model Client

--- clients/urls.py
+++ clients/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from clients.apps import ClientsConfig
-
+from clients.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView
 
 app_name = ClientsConfig.name
 
-# Пустые кавычки - идем от корня
 urlpatterns = [
+    path('', ClientListView.as_view(), name='list'),
+    path('detail/<int:pk>/', ClientDetailView.as_view(), name='detail'),
+    path('create/', ClientCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', ClientUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', ClientDeleteView.as_view(), name='delete'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from clients.forms import ClientForm
+from clients.models import Client
+
+
+class ClientListView(ListView):
+    model = Client
+    extra_context = {
+        'title': 'Клиенты'
+    }
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    extra_context = {
+        'title': 'Информация о клиенте'
+    }
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('clients:list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('clients:list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('clients:list')
